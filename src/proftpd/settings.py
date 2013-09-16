608ff5e6e709d4830e3873cbf76bc9020aa1bd4f
huangyanfeng@pupai.cn <huangyanfeng@pupai.cn>
2013-09-16 18:16:31 +0800

    sd

--- src/proftpd/settings.py
+++ src/proftpd/settings.py
@@ -21,7 +21,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'proftpdadmin',                      # Or path to database file if using sqlite3.
-        'USER': 'proftpdadmin',                      # Not used with sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': '123456',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
