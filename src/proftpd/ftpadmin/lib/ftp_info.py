fa58014c4e694dd437092547759c756a1fc0c2a8
huangyanfeng@pupai.cn <huangyanfeng@pupai.cn>
2013-09-17 10:21:41 +0800

    df

--- src/proftpd/ftpadmin/lib/ftp_info.py
+++ src/proftpd/ftpadmin/lib/ftp_info.py
@@ -1,114 +1,13 @@
-import subprocess
 import re, os
 
 from proftpd.ftpadmin.settings import  FILE_PATH
 from proftpd.ftpadmin.lib.common import is_inside, shell, fix_path, initlog
 
-def get_ftpwho_server_info(line=None):
-    server_info = {}
-    s_re = re.compile(r'^(?P<server_type>.*)\s+\[(?P<server_pid>\d+)\],\sup\sfor\s*(-)?((?P<up_day>\d+)\sday)?(s)?(,)?\s*(-)?((?P<up_hour>\d+)\shr)?(s)?\s+(-)?(?P<up_min>\d+)\smin$')
-    m = s_re.search(line)
-    if hasattr(m, 'group'):
-        server_info['server_type'] = m.group('server_type')
-        server_info['server_pid'] = m.group('server_pid')
-        if m.group('up_day'):
-            server_info['up_day'] = m.group('up_day')
-        else:
-            server_info['up_day'] = 0
-        if m.group('up_hour'):
-            server_info['up_hour'] = m.group('up_hour')
-        else:
-            server_info['up_hour'] = 0
-        server_info['up_min'] = m.group('up_min')
-
-    return server_info
-
-        
-
-def get_ftpwho_user_count(line=None):
-    server_info = {}
-    s_re = re.compile(r'^Service\sclass\s+-\s+(?P<user_count>\d+)\susers$')
-    m = s_re.search(line)
-    server_info['user_count'] = 0
-    if hasattr(m, 'group'):
-        server_info['user_count'] = m.group('user_count')
-
-    return server_info
-
-
-def get_ftpwho_status(ftpwho_list=[]):
-    s_re = re.compile(r'^\s*(?P<pid>\d+)\s(?P<username>.*)\s\[\s(?P<time1>.*)\]\s+(?P<time2>.*)\s(?P<cmd>.*)\sclient:\s(?P<client_ip>.*)\sserver:\s(?P<server_ip>.*)\s\((?P<server_name>.*)\)\sprotocol:\s(?P<protocol>.*)\slocation:\s(?P<location>.*)$')
-    output_list = []
-    for line in ftpwho_list:
-        line_dict = {}
-        m = s_re.search(line)
-        if hasattr(m, 'group'):
-            line_dict['pid'] = m.group('pid')
-            line_dict['username'] = m.group('username')
-            line_dict['time1'] = m.group('time1')
-            line_dict['time2'] = m.group('time2')
-            line_dict['cmd'] = m.group('cmd')
-            line_dict['client_ip'] = m.group('client_ip')
-            line_dict['server_ip'] = m.group('server_ip')
-            line_dict['server_name'] = m.group('server_name')
-            line_dict['protocol'] = m.group('protocol')
-            line_dict['location'] = m.group('location')
-            output_list.append(line_dict)
+class proftpd_base(object):
     
-    return output_list
-
-def get_proftpd_compile_time_settings():
-    pass
-    #./proftpd -V
-
-def get_proftpd_compiled_in_modules():
-    pass
-    #./proftpd -l
-
-def get_proftpd_loaded_modules():
-    pass
-    # ./proftpd -vv
-
-def get_ftp_info():
-    ftp_server_info = {}
-    ftpwho = FILE_PATH.get('ftpwho_path', '/opt/proftpd/bin/ftpwho')
-    if os.path.exists(ftpwho) and os.path.isfile(ftpwho) and os.access(ftpwho, os.X_OK):
-        ftpwho_command = 'LANG=C %s --outform "oneline" -v' %  ftpwho
-        p = subprocess.Popen(ftpwho_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        ftpwho_list = p.stdout.readlines()
-        
-        first_line = ftpwho_list.pop(0)
-        last_line = ftpwho_list.pop()
-    
-        ftp_server_info = get_ftpwho_server_info(line=first_line)
-        ftp_server_info.update( get_ftpwho_user_count(line=last_line) )
-        ftp_server_info['status'] = get_ftpwho_status(ftpwho_list=ftpwho_list)
-
-    return ftp_server_info
-
-
-
-
-
-class proftpd_conf(object):
-    pass
-
-
-
-
-
-
-
-class proftpd_ctrls(object):
-    
-
     def __init__(self, *args, **kwargs):
         self.error_code = 0
         self.proftpd_base_path = fix_path( FILE_PATH.get('proftpd_path', '/opt/proftpd') )
-        self.proftpd_ftpdctl_sock = self.proftpd_base_path + '/var/proftpd.sock'
-        self.ftpdctl_init_parameter = " -s %s " % self.proftpd_ftpdctl_sock
-        self.ftpdctl_path = self.proftpd_base_path + '/bin/ftpdctl'
-        
 
     def error_count(self, error_code=None):
         if error_code != None:
@@ -121,20 +20,219 @@ class proftpd_ctrls(object):
         shell_command_result = self.shell.execute_c(x_command=x_command, x_parameters=x_parameters)
         return shell_command_result
 
-    def debug(self):
-        pass
 
-    def dns(self):
-        pass
+class proftpd_info(proftpd_base):
 
-    def down(self):
-        pass
+    def __init__(self, *args, **kwargs):
+        self.error_code = 0
+        self.proftpd_base_path = fix_path( FILE_PATH.get('proftpd_path', '/opt/proftpd') )
+        self.proftpd_path = self.proftpd_base_path + '/sbin/proftpd'
+        self.ftpwho = FILE_PATH.get('ftpwho_path', self.proftpd_base_path + '/bin/ftpwho')
 
-    def dump(self):
-        pass
+    def compile_time_settings(self):
+        settings_result = self.shell_command(x_command=self.proftpd_path, x_parameters=[' -V'])
+        if self.error_count() < 0:
+            return 'proftpd -V  error'
+        else:
+            return  settings_result
 
-    def get(self):
-        pass
+    def compiled_in_modules(self):
+        compiled_in_modules_result = self.shell_command(x_command=self.proftpd_path, x_parameters=[' -l'])
+        if self.error_count() < 0:
+            return 'proftpd -l  error'
+        else:
+            return  compiled_in_modules_result
+
+    def loaded_modules(self):
+        loaded_modules_result = self.shell_command(x_command=self.proftpd_path, x_parameters=[' -vv'])
+        if self.error_count() < 0:
+            return 'proftpd -vv  error'
+        else:
+            return  loaded_modules_result
+
+    def online_status(self):
+        ftp_server_info = {}
+        k_parameters = []
+        k_parameters.append(' --outform "oneline" -v')
+        
+        ftpwho_restult = []
+        ftpwho_restult = self.shell_command(x_command=self.ftpwho, x_parameters=k_parameters)
+        if self.error_count() < 0:
+            return ' exec ' + self.ftpwho + 'error !'
+        else:
+            ftpwho_list = ftpwho_restult
+            
+            first_line = ftpwho_list.pop(0)
+            last_line = ftpwho_list.pop()
+        
+            ftp_server_info = self.get_ftpwho_server_info(line=first_line)
+            ftp_server_info.update( self.get_ftpwho_user_count(line=last_line) )
+            ftp_server_info['status'] = self.get_ftpwho_status(ftpwho_list=ftpwho_list)
+            return ftp_server_info
+
+
+    def get_ftpwho_server_info(self, line=None):
+        server_info = {}
+        s_re = re.compile(r'^(?P<server_type>.*)\s+\[(?P<server_pid>\d+)\],\sup\sfor\s*(-)?((?P<up_day>\d+)\sday)?(s)?(,)?\s*(-)?((?P<up_hour>\d+)\shr)?(s)?\s+(-)?(?P<up_min>\d+)\smin$')
+        m = s_re.search(line)
+        if hasattr(m, 'group'):
+            server_info['server_type'] = m.group('server_type')
+            server_info['server_pid'] = m.group('server_pid')
+            if m.group('up_day'):
+                server_info['up_day'] = m.group('up_day')
+            else:
+                server_info['up_day'] = 0
+            if m.group('up_hour'):
+                server_info['up_hour'] = m.group('up_hour')
+            else:
+                server_info['up_hour'] = 0
+            server_info['up_min'] = m.group('up_min')
+    
+        return server_info
+    
+            
+    
+    def get_ftpwho_user_count(self, line=None):
+        server_info = {}
+        s_re = re.compile(r'^Service\sclass\s+-\s+(?P<user_count>\d+)\susers$')
+        m = s_re.search(line)
+        server_info['user_count'] = 0
+        if hasattr(m, 'group'):
+            server_info['user_count'] = m.group('user_count')
+    
+        return server_info
+    
+    
+    def get_ftpwho_status(self, ftpwho_list=[]):
+        s_re = re.compile(r'^\s*(?P<pid>\d+)\s(?P<username>.*)\s\[\s(?P<time1>.*)\]\s+(?P<time2>.*)\s(?P<cmd>.*)\sclient:\s(?P<client_ip>.*)\sserver:\s(?P<server_ip>.*)\s\((?P<server_name>.*)\)\sprotocol:\s(?P<protocol>.*)\slocation:\s(?P<location>.*)$')
+        output_list = []
+        for line in ftpwho_list:
+            line_dict = {}
+            m = s_re.search(line)
+            if hasattr(m, 'group'):
+                line_dict['pid'] = m.group('pid')
+                line_dict['username'] = m.group('username')
+                line_dict['time1'] = m.group('time1')
+                line_dict['time2'] = m.group('time2')
+                line_dict['cmd'] = m.group('cmd')
+                line_dict['client_ip'] = m.group('client_ip')
+                line_dict['server_ip'] = m.group('server_ip')
+                line_dict['server_name'] = m.group('server_name')
+                line_dict['protocol'] = m.group('protocol')
+                line_dict['location'] = m.group('location')
+                output_list.append(line_dict)
+        
+        return output_list
+
+
+
+class proftpd_conf(object):
+    pass
+
+
+
+
+class proftpd_ctrls(proftpd_base):
+    
+
+    def __init__(self, *args, **kwargs):
+        self.error_code = 0
+        self.proftpd_base_path = fix_path( FILE_PATH.get('proftpd_path', '/opt/proftpd') )
+        self.proftpd_ftpdctl_sock = self.proftpd_base_path + '/var/proftpd.sock'
+        self.ftpdctl_init_parameter = " -s %s " % self.proftpd_ftpdctl_sock
+        self.ftpdctl_path = self.proftpd_base_path + '/bin/ftpdctl'
+        
+
+    def debug(self, **kwargs):
+        k_parameters=[]
+        k_objective = kwargs.get('k_objective',  [])
+        k_parameters.append('debug')
+        if len(k_objective) > 0 :
+            k_parameters.append(' '.join(k_objective))
+        else:
+            k_parameters.append('config')
+            #k_parameters.append('memory')
+            #k_parameters.append('level')
+
+        debug_result = self.shell_command(x_command=self.ftpdctl_path, x_parameters=k_parameters)
+        
+
+        if self.error_count() < 0:
+            return 'ftpdctl get  error'
+        else:
+            return  debug_result
+
+    def dns(self, **kwargs):
+        k_parameters=[]
+        k_objective = kwargs.get('k_objective',  [])
+        k_parameters.append('dns')
+        if len(k_objective) > 0 :
+            k_parameters.append(' '.join(k_objective))
+        else:
+            k_parameters.append('on')
+            #k_parameters.append('off')
+            #k_parameters.append('clear cache')
+
+        dns_result = self.shell_command(x_command=self.ftpdctl_path, x_parameters=k_parameters)
+        
+        if self.error_count() < 0:
+            return 'ftpdctl get  error'
+        else:
+            return  dns_result
+
+
+    def down(self, **kwargs):
+        k_parameters=[]
+        k_objective = kwargs.get('k_objective',  [])
+        k_parameters.append('down')
+        if len(k_objective) > 0 :
+            k_parameters.append(' '.join(k_objective))
+        else:
+            k_parameters.append('"all"')
+
+        down_result = self.shell_command(x_command=self.ftpdctl_path, x_parameters=k_parameters)
+        
+
+        if self.error_count() < 0:
+            return 'ftpdctl down  error'
+        else:
+            return  down_result
+
+    def dump(self, **kwargs):
+        k_parameters=[]
+        k_objective = kwargs.get('k_objective',  [])
+        k_parameters.append('dump')
+        if len(k_objective) > 0 :
+            k_parameters.append(' '.join(k_objective))
+        else:
+            k_parameters.append('config')
+            #k_parameters.append('memory')
+            #k_parameters.append('classes')
+        dump_result = self.shell_command(x_command=self.ftpdctl_path, x_parameters=k_parameters)
+        
+
+        if self.error_count() < 0:
+            return 'ftpdctl get  error'
+        else:
+            return  dump_result
+
+
+    def get(self, **kwargs):
+        k_parameters=[]
+        k_objective = kwargs.get('k_objective',  [])
+        k_parameters.append('get')
+        if len(k_objective) > 0 :
+            k_parameters.append(' '.join(k_objective))
+        else:
+            k_parameters.append('"config"')
+            #k_parameters.append('"directives"')
+        get_result = self.shell_command(x_command=self.ftpdctl_path, x_parameters=k_parameters)
+        
+
+        if self.error_count() < 0:
+            return 'ftpdctl get  error'
+        else:
+            return  get_result
 
 
     def kick(self, **kwargs):
@@ -172,24 +270,73 @@ class proftpd_ctrls(object):
             return 'invalid k_type or k_objective is null'
  
 
-    def restart(self):
-        pass
+    def restart(self, **kwargs):
+        k_parameters=[]
+        k_objective = kwargs.get('k_objective',  [])
+        k_parameters.append('restart')
+        if len(k_objective) > 0 :
+            k_parameters.append(' '.join(k_objective))
+
+        restart_result = self.shell_command(x_command=self.ftpdctl_path, x_parameters=k_parameters)
+        
+
+        if self.error_count() < 0:
+            return 'ftpdctl restart  error'
+        else:
+            return  restart_result
+
 
     def scoreboard(self):
         pass
 
-    def shutdown(self):
-        pass
 
-    def status(self):
-        status_result = self.shell_command(x_command=self.ftpdctl_path, x_parameters=['status  all'])
-        return status_result
+    def shutdown(self, **kwargs):
+        k_parameters = []
+        k_parameters.append('shutdown')
+        shutdown_result = self.shell_command(x_command=self.ftpdctl_path, x_parameters=k_parameters)
+        
+        if self.error_count() < 0:
+            return 'ftpdctl shutdown  error'
+        else:
+            return  shutdown_result
+
+
+    def status(self, **kwargs):
+
+        k_type = kwargs.get('k_type', None)
+        k_objective = kwargs.get('k_objective',  [])
+        k_parameters = ['status']
+        if len(k_objective) > 0 :
+            k_parameters.append(' '.join(k_objective))
+        else:
+            k_parameters.append('"all"')
+
+        status_result = self.shell_command(x_command=self.ftpdctl_path, x_parameters=k_parameters)
+        if self.error_count() < 0:
+            return 'ftpdctl status  error'
+        else:
+            return  status_result
+
 
     def trace(self):
         pass
 
-    def up(self):
-        pass
+    def up(self, **kwargs):
+        k_parameters=[]
+        k_objective = kwargs.get('k_objective',  [])
+        k_parameters.append('up')
+        if len(k_objective) > 0 :
+            k_parameters.append(' '.join(k_objective))
+        else:
+            k_parameters.append('0.0.0.0')
+
+        up_result = self.shell_command(x_command=self.ftpdctl_path, x_parameters=k_parameters)
+        
+
+        if self.error_count() < 0:
+            return 'ftpdctl up  error'
+        else:
+            return  up_result
 
 
 
